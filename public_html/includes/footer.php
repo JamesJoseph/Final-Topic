3de3683926e2f3ef6e854b6275157111026a08b2
James Joseph <jim@jimsrightbrain.com>
2013-05-13 00:30:27 -0400

    Final-Topic
    added all files - comps, sketches, wireframes, and validated code

---
+++ public_html/includes/footer.php
@@ -0,0 +1,27 @@
+			
+
+			<footer class="footer">	
+				
+			</footer>
+
+			
+
+		
+
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		
+		<script type="text/javascript" src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+
+
+
+		<script src="js/tabs.js"></script>
+
+		<script src="js/tooltip.js"></script>
+
+		<script src="js/features-accordion.js"></script>
+
+		<script type="text/javascript" src="js/modernizr.js"></script>
+
+	</body>
+
+</html>
---
+++ public_html/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		
+		<title>PhoneGap</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/fonts.css">
+		<link rel="stylesheet" type="text/css" href="css/topic-main.css">
+
+		<link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>	
+
+		
+
+	</head>
+
+	<body>
+
+		<div id="header-wrap">
+
+			<header id="logo">
+
+				<a href="index.php"><img src="images/phonegap-logo.png" alt="phonegap logo" width="125" height="135"></a>
+
+			</header>
+
+		</div>
